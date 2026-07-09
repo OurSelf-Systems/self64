484c4e43f3752014df17fa97c3b5d96c6736caa5
Russell Allen <mail@russell-allen.com>
2026-07-09 18:26:53 +1000

    amd64 SIC: rewrite regs_amd64 for x86-64, mapping regs_aarch64's roles onto the 14 usable GPRs: rax=Result/NLRResult, rdx/rcx/rsi=NLR home/homeID/temp, TempRegs={rdi,rbx,r8,r9}, Temp1-4=r10/r11/r14/r15, Perform locs=r12/r13 (callee-saved, survive the C lookup call), FrameReg=rbp. Enum values are ModRM hardware numbers so Locations feed the encoders directly; legacy eax/ebp aliases keep the shared frame code's x86 paths compiling. No assembler-reserved scratch regs: disp32 addressing and [rip+disp32] sends need none (aarch64 reserves x16/x17).

--- vm64/src/amd64/asm/regs_amd64.cpp
+++ vm64/src/amd64/asm/regs_amd64.cpp
@@ -1,67 +1,74 @@
-# if  TARGET_ARCH == I386_ARCH || TARGET_ARCH == X86_64_ARCH
-/* Sun-$Revision: 1.4 $ */
-
-/* Copyright 1992-2012 AUTHORS.
+# if TARGET_ARCH == X86_64_ARCH
+/* Copyright 1992-2026 AUTHORS.
    See the LICENSE file for license information. */
-  
+
 # pragma implementation "regs_amd64.hh"
-  
+
 # include "_regs_amd64.cpp.incl"
 
-const char** RegisterNamesBySize[] = {ByteRegisterNames, ShortRegisterNames, RegisterNames};
-
-// change Location enum in regs_i386.h if you change this!
+// change Location enum in regs_amd64.hh if you change this!
 const char* RegisterNames[] = {
-    "%eax", "%ecx", "%edx", "%ebx", "%esp", "%ebp", "%esi", "%edi",
+    "rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi",
+    "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
     "",
-    
+
     "*UnAllocated*"
 };
 const char* ByteRegisterNames[] = {
-    "%al", "%cl", "%dl", "%bl", "%ah", "%ch", "%dh", "%bh",
+    "al",  "cl",  "dl",  "bl",  "spl", "bpl", "sil", "dil",
+    "r8b", "r9b", "r10b", "r11b", "r12b", "r13b", "r14b", "r15b",
     "",
-    
+
     "*UnAllocated*"
 };
 const char* ShortRegisterNames[] = {
-    "%ax", "%cx", "%dx", "%bx", "%sp", "%bp", "%si", "%di",
+    "ax",  "cx",  "dx",  "bx",  "sp",  "bp",  "si",  "di",
+    "r8w", "r9w", "r10w", "r11w", "r12w", "r13w", "r14w", "r15w",
     "",
-    
+
     "*UnAllocated*"
 };
-    
-    
 
+const char** RegisterNamesBySize[] =
+  { ByteRegisterNames, ShortRegisterNames, RegisterNames };
 
 
 const char *locationName(Location l) {
   const char* c;
   int num;
-  
-       if          ( is_IArgLocation(l)) {   c = "I";  num = index_for_IArgLocation(l); }
-  else if          ( is_LArgLocation(l)) {   c = "L";  num = index_for_LArgLocation(l); }
-  else if          ( is_ArgLocation(l)) {    c = "";   num = index_for_ArgLocation(l); }
-  else if          ( is_StackLocation(l)) { c = "S";  num = index_for_StackLocation(l); }
-  else if          ( l == IReceiverReg ) { c = "I"; num = -1; }
-  else if          ( l == LReceiverReg ) { c = "L"; num = -1; }
-  else if          ( l == ReceiverReg ) { c = "R"; num = -1; }
+
+       if ( is_IArgLocation(l)) {   c = "I";  num = index_for_IArgLocation(l); }
+  else if ( is_LArgLocation(l)) {   c = "L";  num = index_for_LArgLocation(l); }
+  else if ( is_ArgLocation(l))  {   c = "";   num = index_for_ArgLocation(l); }
+  else if ( is_StackLocation(l)){   c = "S";  num = index_for_StackLocation(l); }
+  else if ( l == IReceiverReg ) {   c = "I";  num = -1; }
+  else if ( l == LReceiverReg ) {   c = "L";  num = -1; }
+  else if ( l == ReceiverReg )  {   c = "R";  num = -1; }
   else {
     assert(isRegister(l), "");
     return RegisterNames[l];
   }
-  char* s= new char [30]; // known leak NEW_RESOURCE_ARRAY(char, 30);
+  char* s = new char[30];
   sprintf(s, "%s%ld", c, long(num));
   return s;
 }
-    
+
+
+void printMask(RegisterString mask) {
+  assert(mask == 0, "unused for amd64");
+}
+
 
 # if defined(SIC_COMPILER)
-  Location TempRegs[] = { eax, edx, esi, edi };
-    
+
+  Location TempRegs[] = { rdi, rbx, r8, r9 };
+
 # define X(arg) -99999999     /* to make the following table look nicer */
   fint RegToTempNo[/* indexed by Location */] = {
-    0, X("ecx"), 1, X("ebx"), X("esp"), X("ebp"), 2, 3 
+    X("rax"), X("rcx"), X("rdx"), 1, X("rsp"), X("rbp"), X("rsi"), 0,
+    2, 3, X("Temp1"), X("Temp2"), X("r12"), X("r13"), X("Temp3"), X("Temp4")
   };
+# undef X
 
   Location CalleeSavedRegs[] = {
   };
@@ -69,7 +76,7 @@ const char *locationName(Location l) {
   void regs_amd64_init() {
     // The SIC uses Temp1 and Temp2 during code generation, that's why they
     // aren't listed as general temp regs above.
-    assert(Temp1 == ebx && Temp2 == ecx, "change this");
+    assert(Temp1 == r10 && Temp2 == r11, "change this");
     for (fint i = 0; i < NumTempRegs; i++) {
       assert(TempRegs[i] != Temp1 && TempRegs[i] != Temp2,
              "Temp1 and Temp2 are reserved for the code generator");
@@ -79,7 +86,14 @@ const char *locationName(Location l) {
 
 # else
 
-void regs_amd64_init() {}
+  Location TempRegs[] = {};
+  fint RegToTempNo[] = {};
+  Location CalleeSavedRegs[] = {};
 
-# endif
-# endif // TARGET_ARCH == I386_ARCH
+  void regs_amd64_init() {
+    // No JIT register setup without SIC_COMPILER
+  }
+
+# endif // defined(SIC_COMPILER)
+
+# endif // TARGET_ARCH == X86_64_ARCH
